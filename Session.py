8b09cdd7e6fbd987fd6f5a19bfaa92d73d792134
Kyle McCarthy <km@kylemccarthy.io>
2016-02-26 15:26:21 -0600

    session and student fields

---
+++ Session.py
@@ -0,0 +1,14 @@
+class Session:
+
+    def __init__(self, id, exercise, activity, start_time, end_time, idle_time, mouse_wheel, mouse_wheel_click, mouse_click_left, mouse_click_right, mouse_movement, keystroke):
+        self.id = id
+        self.exercise = exercise
+        self.activity = activity
+        self.start_time = start_time
+        self.end_time = end_time
+        self.idle_time = idle_time
+        self.mouse_wheel = mouse_wheel
+        self.mouse_click_left = mouse_click_left
+        self.mouse_click_right = mouse_click_right
+        self.mouse_movement = mouse_movement
+        self.keystroke = keystroke
---
+++ Student.py
@@ -0,0 +1,8 @@
+from Session import Session
+
+
+class Student:
+
+    def __init__(self, id):
+        self.id = id
+        self.sessions = []
