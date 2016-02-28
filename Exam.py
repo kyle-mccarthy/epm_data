a115b3e0033e3ad1bc8ada454cea90432d234fda
Kyle McCarthy <km@kylemccarthy.io>
2016-02-28 13:49:11 -0600

    we need an exam object to store the data

---
+++ Exam.py
@@ -0,0 +1,23 @@
+class Exam:
+
+    def __init__(self, id, es_1_1, es_1_2, es_2_1, es_2_2, es_3_1, es_3_2, es_3_3, es_3_4, es_3_5, es_4_1, es_4_2, 
+                 es_5_1, es_5_2, es_5_3, es_6_1, es_6_2, total):
+        # there were two different possible exams
+        self.id = id
+        self.es_1_1 = es_1_1
+        self.es_1_2 = es_1_2
+        self.es_2_1 = es_2_1
+        self.es_2_2 = es_2_2
+        self.es_3_1 = es_3_1
+        self.es_3_2 = es_3_2
+        self.es_3_3 = es_3_3
+        self.es_3_4 = es_3_4
+        self.es_3_5 = es_3_5
+        self.es_4_1 = es_4_1
+        self.es_4_2 = es_4_2
+        self.es_5_1 = es_5_1
+        self.es_5_2 = es_5_2
+        self.es_5_3 = es_5_3
+        self.es_6_1 = es_6_1
+        self.es_6_2 = es_6_2
+        self.total = total
